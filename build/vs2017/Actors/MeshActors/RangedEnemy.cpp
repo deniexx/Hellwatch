10cb3b646a23dbd8299665a7349cf12b63318076
TorinPellegrini <93349773+TorinPell@users.noreply.github.com>
2023-04-06 16:01:26 +0100

    Updated Ranged Enemy

--- build/vs2017/Actors/MeshActors/RangedEnemy.cpp
+++ build/vs2017/Actors/MeshActors/RangedEnemy.cpp
@@ -51,7 +51,9 @@ void RangedEnemy::Update(float deltaTime)
 {
 	Super::Update(deltaTime);
 
-	Shoot();
+	if (attackTime + attackCooldown < SceneApp::instance->GetCurrentGameTime()) {
+		Shoot();
+	}
 
 	PlayerCharacter* player = SceneApp::instance->GetPlayerCharacter();
 	gef::Vector4 playerPosition = player->GetTranslation();
@@ -75,47 +77,45 @@ void RangedEnemy::Update(float deltaTime)
 }
 
 void RangedEnemy::Shoot() {
-	if (attackTime + attackCooldown < SceneApp::instance->GetCurrentGameTime()) {
 		//Super::Begin();
+	attackTime = SceneApp::instance->GetCurrentGameTime();
+	PlayerCharacter* player = SceneApp::instance->GetPlayerCharacter();
+	gef::Vector4 playerPosition = player->GetTranslation();
+	b2Vec2 playerDirection = b2Vec2(playerPosition.x(), playerPosition.z());
+	gef::Vector4 enemyPosition = GetTranslation();
+	b2Vec2 enemyDirection = b2Vec2(enemyPosition.x(), enemyPosition.z());
+	b2Vec2 towardsPlayer = playerDirection - enemyDirection;
+	towardsPlayer.Normalize();
 
-		PlayerCharacter* player = SceneApp::instance->GetPlayerCharacter();
-		gef::Vector4 playerPosition = player->GetTranslation();
-		b2Vec2 playerDirection = b2Vec2(playerPosition.x(), playerPosition.z());
-		gef::Vector4 enemyPosition = GetTranslation();
-		b2Vec2 enemyDirection = b2Vec2(enemyPosition.x(), enemyPosition.z());
-		b2Vec2 towardsPlayer = playerDirection - enemyDirection;
-		towardsPlayer.Normalize();
+	gef::Vector4 target = playerPosition - enemyPosition;
+	target.Normalise();
+	translation = enemyPosition + target * 2.0f;
 
-		gef::Vector4 target = playerPosition - enemyPosition;
-		target.Normalise();
-		translation = enemyPosition + target;
-
-		gef::Mesh* mesh = SceneApp::instance->GetPrimitiveBuilder()->CreateBoxMesh(gef::Vector4(0.2f, 0.2f, 0.2f));
-		DamageOnCollisionActor* actor = SceneApp::instance->SpawnMeshActor<DamageOnCollisionActor>(mesh, translation);
-		actor->SetDamageAmount(10);
-		gef::Material mat;
-		mat.set_colour(0xFFC18B36);
-		actor->SetMaterial(mat);
+	gef::Mesh* mesh = SceneApp::instance->GetPrimitiveBuilder()->CreateBoxMesh(gef::Vector4(0.2f, 0.2f, 0.2f));
+	DamageOnCollisionActor* actor = SceneApp::instance->SpawnMeshActor<DamageOnCollisionActor>(mesh, translation);
+	actor->SetDamageAmount(10);
+	gef::Material mat;
+	mat.set_colour(0xFFC18B36);
+	actor->SetMaterial(mat);
 
 
-		b2BodyDef bodyDef;
-		bodyDef.type = b2_dynamicBody;
-		bodyDef.position = b2Vec2(translation.x(), translation.z());
-		bodyDef.enabled = true;
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position = b2Vec2(translation.x(), translation.z());
+	bodyDef.enabled = true;
 
-		b2PolygonShape shape;
-		shape.SetAsBox(0.4f, 0.4f);
+	b2PolygonShape shape;
+	shape.SetAsBox(0.4f, 0.4f);
 
-		b2FixtureDef fixtureDef;
-		fixtureDef.shape = &shape;
-		fixtureDef.density = 1.f;
-		fixtureDef.friction = 0.75f;
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &shape;
+	fixtureDef.density = 1.f;
+	fixtureDef.friction = 0.75f;
 
-		b2Body* body = SceneApp::instance->CreateCollisionBody(bodyDef, fixtureDef, actor);
-		actor->SetCollisionBody(body);
-		b2Vec2 forceDir = b2Vec2(target.x(), target.z());
-		forceDir *= 2000.f;
+	b2Body* body = SceneApp::instance->CreateCollisionBody(bodyDef, fixtureDef, actor);
+	actor->SetCollisionBody(body);
+	b2Vec2 forceDir = b2Vec2(target.x(), target.z());
+	forceDir *= 1000.f;
 
-		actor->GetCollisionBody()->ApplyForceToCenter(forceDir, true);
-	}
+	actor->GetCollisionBody()->ApplyForceToCenter(forceDir, true);
 }
